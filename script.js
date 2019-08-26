3ac9a942b67cdb11f57572f2ba884a00d4eb3362
Moshe Elkalay <36788449+moshiko1991@users.noreply.github.com>
2019-08-26 10:51:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("into js");
+
+
+
+
+	function prive(){
+		var getValue = document.getElementById('private').value;
+
+	    console.log(getValue);
+
+	    var open = window.open('Y:/DATA/SUPPORT/!PRIVATE/' + getValue);
+	}
+
+
+	function ping(){
+		var getNum = document.getElementById('ping').value;
+
+	    console.log(getNum);
+
+	    var src = s.match(value);
+
+	    var open = window.open('Y:/DATA/SUPPORT/Client Connections/@---TOUCH---@' + src);
+	}
+
+
+
+		
+
+	
+
+	
+	
+
+
+
+
+
+
+	
+	
+	
+	
+
